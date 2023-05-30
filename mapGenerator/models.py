e19228156b34d7b7e3a938035b3666bcfa5d5c19
Vinícius Pires <viniciusjps15@gmail.com>
2023-05-29 21:14:46 -0300

    v1.2

--- mapGenerator/models.py
+++ mapGenerator/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+## Representa um Link (Endereço Web).
+class Link(models.Model):
+    nome = models.CharField(max_length = 255)
+    url = models.CharField(max_length = 500)
+
+    def __str__(self):
+        return f'{self.nome} = {self.url}'
