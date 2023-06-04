b87652cbf7b7746f76229bc9742292e910c243e1
YuryOAraujo <yuryaraujo02@gmail.com>
2023-06-04 19:58:06 -0300

    Criação da pasta 'static'

--- homePage/urls.py
+++ homePage/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 app_name = 'homePage'
 
 urlpatterns = [
-    path('', views.index,name='index'),
+    path('', views.index, name='index'),
 ]
