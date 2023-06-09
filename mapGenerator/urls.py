94eab7cae794c211a839f0881190691ab9e64139
Vinícius Pires <viniciusjps15@gmail.com>
2023-06-09 10:42:47 -0300

    Download do XML
    Adicionadas a função de criação e download do XML à view, tratamento da url no front.
    Ainda há erros para urls do estilo: "https://link.com.br"

--- mapGenerator/urls.py
+++ mapGenerator/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path,include
+from django.urls import path
 from . import views
+
 app_name = 'mapGenerator'
 
 urlpatterns = [
-    path('', views.index,name='index'),
-    path('geradorDeMapaDeSite', views.index,name='index'),
-    path('resultado?url=<str:url>', views.resultPage,name='result'),
-    path('resultado?', views.resultPage,name='result'),
+    path('', views.index, name='index'),
+    path('geradorDeMapaDeSite', views.index, name='index'),
+    path('resultado?url=<str:url>', views.resultPage, name='result'),
+    path('resultado?', views.resultPage, name='result'),
+    path('download-xml?url=<str:url>', views.download_xml, name='download-xml'),
 
 ]
--- mapGenerator/views.py
+++ mapGenerator/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .link_functions import get_links
 from .clusterization import clusterize
+from django.http import HttpResponse
+
+import xml.etree.ElementTree as ET
 import json
+
 def index(request):
     TEMPLATE_NAME = 'mapGenerator/site.html'
     return render(request,TEMPLATE_NAME)
@@ -13,3 +17,21 @@ def resultPage(request, url=''):
     clusters_json = json.dumps(clusters)
     # print(clusters_json)
     return render(request,TEMPLATE_NAME,{'clusters_json' : clusters_json,})  
+
+
+def download_xml(request, url=''):
+    '''
+    Função responsável por gerar um arquivo XML com o mapa do site.
+    '''
+    root = ET.Element("root")
+    child = ET.SubElement(root, "child")
+    child.text = "Conteúdo do XML"
+    tree = ET.ElementTree(root)
+
+    response = HttpResponse(content_type='application/xml')
+    response['Content-Disposition'] = 'attachment; filename="arquivo.xml"'
+
+    # Salva o arquivo XML na resposta (response)
+    tree.write(response, encoding='utf-8', xml_declaration=True)
+
+    return response
