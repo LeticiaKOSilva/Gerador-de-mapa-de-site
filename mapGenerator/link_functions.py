3065b4b61671177e98bce06795d7a1d011282e09
Vinícius Pires <viniciusjps15@gmail.com>
2023-06-08 21:22:19 -0300

    Função de ordenação pela ordem de ocorrência
    Relevância Presumida

--- mapGenerator/link_functions.py
+++ mapGenerator/link_functions.py
@@ -1,6 +1,7 @@
 from tokenize import String
 import requests
 from bs4 import BeautifulSoup
+from collections import defaultdict
 from .models import Link
 ##
 #
@@ -35,7 +36,7 @@ def create_request(url, ssl_cert=True): #
         return requests.get(url, verify=False)
     except (requests.exceptions.ConnectionError): # Tratamento caso a página não exista.
         return None
-    except (requests.exceptions.MissingSchema): # Tratamento caso a url esteja em formato inválido
+    except (requests.exceptions.MissingSchema): # Caso a url esteja em formato inválido
         return None
 #
 
@@ -86,18 +87,43 @@ def extract_urls(links: list[Link]):
 
 def get_content(url: String):
     """
-    Recebe uma url e retorna todo o HTML contido nela ou String vazia caso a requisição não seja bem sucedida
+    Recebe uma url e retorna todo o HTML contido nela 
+    ou String vazia caso a requisição não seja bem sucedida
     """
     response = create_request(url)
-    if(response == None):
+    if(response is None):
         return ""
     return response.text
 
 def remove_url_prefix(url: str):
     """
-    Remove a parte inicial de uma url e retorna a url sem esse inicio ou string vazia caso não consiga realizar o split
+    Remove a parte inicial de uma url e retorna a url sem esse 
+    inicio ou string vazia caso não consiga realizar o split
     Ex: url = https://github.com/login/, return url = login/
     
     """
     parts = url.split('.com')
     return parts[1] if len(parts) > 1 else ""
+#
+
+def get_links_sorted_by_occurrence(url: str):
+    '''
+    Obtém uma lista de links ordenados pela sua 
+    ocorrência na página em ordem decrescente.
+    '''
+    return sort_links_by_occurrence(get_links(url))
+
+def sort_links_by_occurrence(links):
+    '''
+    Ordena uma lista de links baseando-se no número de ocorrências na própria lista.
+    '''
+    dictionary = defaultdict(int)
+    
+    # Contagem das ocorrências dos links
+    for link in links:
+        dictionary[link] += 1
+    
+    # Ordenação decrescente por contagem de ocorrência
+    links_count = sorted(dictionary.items(), key=lambda x: x[1], reverse=True)
+    
+    return links_count
